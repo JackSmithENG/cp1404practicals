9ff91636a7d00338f9201a3de03e88baac5e5bf7
JackSmithENG <jack.smith1@my.jcu.edu.au>
2019-09-04 12:21:59 +1000

    Format all exercises

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,5 +1,5 @@
 HEX_COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2":
-               "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4}",
+    "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4}",
                "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
 
 colour_name = input("Name of colour: ")
