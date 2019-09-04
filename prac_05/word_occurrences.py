e6577dc00c4e23812a5d1a751954c1333b6ec244
JackSmithENG <jack.smith1@my.jcu.edu.au>
2019-09-04 12:44:48 +1000

    Format state_names and word_occurences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,15 +1,15 @@
 text = input("Text: ")
-text_dict = {}
+word_to_count = {}
 words = text.split()
 for word in words:
-    if word in text_dict:
-        text_dict[word] += 1
+    if word in word_to_count:
+        word_to_count[word] += 1
     else:
-        text_dict[word] = 1
+        word_to_count[word] = 1
 
-words = list(text_dict.keys())
+words = list(word_to_count.keys())
 words.sort()
 
 maximum_length = max((len(word) for word in words))
 for word in words:
-    print("{:{}} : {}".format(word, maximum_length, text_dict[word]))
+    print("{:{}} : {}".format(word, maximum_length, word_to_count[word]))
