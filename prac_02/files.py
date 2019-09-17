b91a24dec99fa24f25215b201464358fa5ab63ae
JackSmithENG <jack.smith1@my.jcu.edu.au>
2019-09-17 19:37:59 +1000

    Add completed used.cars and car files

--- prac_02/files.py
+++ prac_02/files.py
@@ -1,21 +1,21 @@
-OUTPUT_FILE_NAME = 'name.txt'  # Name the output file
-out_file_name = open(OUTPUT_FILE_NAME, 'w')  # Write to output file
-
-user_name = input("Enter your name: ")
-print(user_name.format(), file=out_file_name)
-
-in_file_name = open('name.txt', 'r')
-name = in_file_name.read().strip()
-print("Your name is", user_name, file=out_file_name)
-in_file_name.close()
-
-in_file_numbers = open('numbers.txt', 'r+')
-number1 = int(in_file_numbers.readline())
-number2 = int(in_file_numbers.readline())
-sum_of_file = number1 + number2
-print("Sum = {}".format(sum_of_file), file=in_file_numbers)
-in_file_numbers.close()
-out_file_name.close()
+# OUTPUT_FILE_NAME = 'name.txt'  # Name the output file
+# out_file_name = open(OUTPUT_FILE_NAME, 'w')  # Write to output file
+#
+# user_name = input("Enter your name: ")
+# print(user_name.format(), file=out_file_name)
+#
+# in_file_name = open('name.txt', 'r')
+# name = in_file_name.read().strip()
+# print("Your name is", user_name, file=out_file_name)
+# in_file_name.close()
+#
+# in_file_numbers = open('numbers.txt', 'r+')
+# number1 = int(in_file_numbers.readline())
+# number2 = int(in_file_numbers.readline())
+# sum_of_file = number1 + number2
+# print("Sum = {}".format(sum_of_file), file=in_file_numbers)
+# in_file_numbers.close()
+# out_file_name.close()
 
 in_file_multiple_numbers = open('multiple_numbers.txt', 'r+')
 finished = False
